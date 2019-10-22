a3e750009f18c5e8a4b79bacab5a67aef555ddd6
Guy Ellis <wildfiction@gmail.com>
2019-10-21 21:06:06 -0700

    Move types into index file. Issue #150

--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,59 @@ import {
   logSingle,
 } from './loggly-wrapper';
 
+
+type LevelEnum =
+  'trace'|
+  'info'|
+  'warn'|
+  'error'|
+  'fatal'|
+  'security';
+
+interface LogPresets {
+  [key: string]: string;
+}
+
+interface LaLogOptions {
+  addTrackId?: boolean;
+  moduleName?: string;
+  presets?: LogPresets;
+  serviceName?: string;
+  isTransient?: boolean;
+}
+
+interface TimeEndLog {
+  (label: string, extraLogDat?: any): Promise<any>;
+  trace?: (label: string, extraLogDat?: any) => Promise<any>;
+  info?: (label: string, extraLogDat?: any) => Promise<any>;
+  warn?: (label: string, extraLogDat?: any) => Promise<any>;
+  error?: (label: string, extraLogDat?: any) => Promise<any>;
+  fatal?: (label: string, extraLogDat?: any) => Promise<any>;
+  security?: (label: string, extraLogDat?: any) => Promise<any>;
+}
+
+interface LaLog{
+  new(options: LaLogOptions): void;
+
+  // Static methods
+  create: (options: LaLogOptions) => LaLog;
+  setLevel: (level: LevelEnum) => LevelEnum;
+  getLevel: () => LevelEnum;
+  allLevels: () => Array<LevelEnum>;
+
+  // Instance methods
+  time: (label: string) => void;
+  // TODO: timeEnd has all the log methods on it.
+  timeEnd: (label: string, extraLogData?: object) => Promise<undefined>;
+  trace: (logObj: object) => Promise<undefined>;
+  info: (logObj: object) => Promise<undefined>;
+  warn: (logObj: object) => Promise<undefined>;
+  error: (logObj: object) => Promise<undefined>;
+  fatal: (logObj: object) => Promise<undefined>;
+  security: (logObj: object) => Promise<undefined>;
+}
+
+
 const levels: LevelEnum[] = ['trace', 'info', 'warn', 'error', 'fatal', 'security'];
 const errorLevel = levels.indexOf('error');
 

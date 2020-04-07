93fd94cfacba91f6592d4ce40c50d4c267255496
Guy Ellis <wildfiction@gmail.com>
2020-04-07 06:06:53 -0700

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,11 @@ module.exports = {
     'import/no-unresolved': [0],
     "@typescript-eslint/no-explicit-any": [0],
     "import/prefer-default-export": [0],
+    'import/extensions': [2, {
+        ts: 'never', // Never require .ts extensions
+        json: 'always', // Always require .json extensions
+      }
+    ],
     "jest/no-disabled-tests": [2],
     "jest/no-focused-tests": [2],
     "jest/no-identical-title": [2],
--- test/lib/utils.test.ts
+++ test/lib/utils.test.ts
@@ -2,11 +2,14 @@ import { isObject } from '../../lib/utils';
 
 describe('utils', () => {
   test('isObject', () => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const emptyArrow = (): void => {};
+
     expect(isObject({})).toBe(true);
     expect(isObject(1 as unknown as object)).toBe(false);
     expect(isObject(true as unknown as object)).toBe(false);
     expect(isObject(null)).toBe(false);
-    expect(isObject(() => {})).toBe(false);
+    expect(isObject(emptyArrow)).toBe(false);
     expect(isObject([1, 2])).toBe(false);
     expect(isObject([])).toBe(false);
   });

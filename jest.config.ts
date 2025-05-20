4936479f717ddbe95c3f45b38e6e9939ab7e2954
Guy Ellis <wildfiction@gmail.com>
2025-05-20 10:40:41 -0700

    Change jest.config from a JS file to a TS file

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,8 @@
+import type { Config } from 'jest';
+
 const collectCoverage = !process.env.SKIP_COVERAGE;
 
-/** @type {jest.InitialOptions} */
-module.exports = {
+const config: Config = {
   collectCoverage,
   collectCoverageFrom: [
     '!**/.vscode/**',
@@ -36,3 +37,5 @@ module.exports = {
   },
   verbose: false,
 };
+
+export default config;
